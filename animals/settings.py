583549c6ab7b718861fd730ba0c29af12b1fb958
f-nagel-1 <59820109+f-nagel-1@users.noreply.github.com>
2021-11-21 18:56:37 +0200

    Update settings.py

--- animals/settings.py
+++ animals/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q-jh17bi#3ed=qurxnn7%cr-0m^0+-rbyd5)p=$u9n6qqt(7g^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
